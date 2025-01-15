3a63419a80bfdf64d56a219be70f1b9fbb8e0610
DESKTOP-PCGDFF5\cuong <cuongdola10062001@gmail.com>
2025-01-15 22:07:37 +0700

    refactor

--- Assets/_FPSShooter/Script/Bullet/BulletCtrl.cs
+++ Assets/_FPSShooter/Script/Bullet/BulletCtrl.cs
@@ -2,5 +2,38 @@ using UnityEngine;
 
 public class BulletCtrl : ResetMonoBehaviour
 {
-    
+    [SerializeField] protected Collider cd;
+    public Collider Cd=> cd;
+
+    [SerializeField] protected Rigidbody rb;
+    public Rigidbody Rb => rb;
+
+
+    #region LoadComponents
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+
+        this.LoadCollider();
+        this.LoadRigidbody();
+    }
+
+    protected virtual void LoadCollider()
+    {
+        if (this.cd != null) return;
+
+        this.cd = GetComponent<Collider>();
+        Debug.LogWarning(transform.name + ": LoadCollider", gameObject);
+    }
+
+    protected virtual void LoadRigidbody()
+    {
+        if (this.rb != null) return;
+
+        this.rb = GetComponent<Rigidbody>();
+        Debug.LogWarning(transform.name + ": LoadRigidbody", gameObject);
+    }
+    #endregion
+
+
 }
